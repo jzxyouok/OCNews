fe826bbd2a00e98cefa4df35faadd191767268cc
lvchengqiao <lvchengqiao@163.com>
2016-10-18 23:16:32 +0800

    新建项目

---
+++ OCNews/OCNews/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  OCNews
+//
+//  Created by 吕成翘 on 16/10/18.
+//  Copyright © 2016年 Apress. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ OCNews/OCNews/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  OCNews
+//
+//  Created by 吕成翘 on 16/10/18.
+//  Copyright © 2016年 Apress. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
